81b87e7731d3181312a9f147965a6e35700e4141
Matt Krukowski <matthew@chartbeat.com>
2015-11-12 13:54:04 -0500

    deploy to clojars

--- project.clj
+++ project.clj
@@ -1,18 +1,15 @@
-(defproject com.chartbeat.cljbeat/options "1.0.0-SNAPSHOT"
+(defproject com.chartbeat.cljbeat/options "1.0.0"
   :description "Chartbeat specific option parsing utility functions."
+  :url "https://github.com/chartbeat-labs/cljbeat-options"
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/license/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.logging "0.3.1"]
                  [clj-yaml "0.4.0"]]
 
-  :plugins [[s3-wagon-private "1.1.2"]]
-
-  :repositories [["releases" {:url "https://jars.chartbeat.com:7443/releases/"}]
-                 ["snapshots" {:url "https://jars.chartbeat.com:7443/snapshots/"}]]
-  :deploy-repositories [["releases" {:url "s3p://chartbeat-jars/releases/"
-                                     :sign-releases false}]
-                        ["snapshots" {:url "s3p://chartbeat-jars/snapshots/"
-                                      :sign-releases false}]]
+  :deploy-repositories [["releases" :clojars]]
+  :signing {:gpg-key "F0903068"}
 
   :aot :all
   :vcs :git)
